b07ce93be13fb436a42c32cf67f189852dbfcd8b
Anders Tøgersen <au563513@uni.au.dk>
2020-01-29 15:46:52 +0100

    Added Accumulator functionality

--- Calculator/Calculator/Calculator.cs
+++ Calculator/Calculator/Calculator.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml.Serialization;
 
 namespace Calculator
 {
@@ -8,22 +9,61 @@ namespace Calculator
     {
         public double Add(double a, double b)
         {
-            return a + b;
+            return Accumulator = a + b;
+        }
+
+        public double Add(double a)
+        {
+            return Accumulator += a;
         }
 
         public double Subtract(double a, double b)
         {
-            return a - b;
+            return Accumulator = a - b;
+        }
+
+        public double Subtract(double a)
+        {
+            return Accumulator -= a;
         }
 
         public double Multiply(double a, double b)
         {
-            return a * b;
+            return Accumulator = a * b;
+        }
+
+        public double Multiply(double a)
+        {
+            return Accumulator *= a;
         }
 
         public double Power(double a, double exp)
         {
-            return Math.Pow(a, exp);
+            return Accumulator = Math.Pow(a, exp);
         }
+
+        public double Power(double exp)
+        {
+            return Accumulator = Math.Pow(Accumulator, exp);
+        }
+
+        public double Divide(double devidend, double divisor)
+        {
+            if (divisor == 0)
+            {
+                throw new DivideByZeroException("Domain error");
+            }
+
+            return devidend / divisor;
+        }
+
+        public double Accumulator { get; private set; } = 0;
+
+        public void Clear()
+        {
+            Accumulator = 0;
+        }
+
     }
+
 }
